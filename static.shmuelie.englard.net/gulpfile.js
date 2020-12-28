677e820a45ef6444ffb7212d6041e0a08835e6d6
Shmueli Englard <shmueli.yosef@englard.net>
2020-12-28 14:26:28 -0500

    Added missing content

---
+++ static.shmuelie.englard.net/gulpfile.js
@@ -0,0 +1,17 @@
+﻿var gulp = require("gulp");
+var ts = require("gulp-typescript");
+var tsProject = ts.createProject("tsconfig.json");
+
+gulp.task("ts-build", function () {
+    return tsProject.src().pipe(tsProject()).js.pipe(gulp.dest("dist"));
+});
+
+var paths = {
+    pages: ["www/*.htm", "www/*.css"]
+};
+
+gulp.task("copy-static", function () {
+    return gulp.src(paths.pages).pipe(gulp.dest("dist"));
+});
+
+gulp.task("build-Debug", gulp.series(["ts-build", "copy-static"]));
