3056fa007258b86a0e54e8b4efb5b471cca21ec7
Shmueli Englard <shmueli.yosef@englard.net>
2021-01-29 13:43:02 -0500

    Created & added to page widget to link to repo

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 ﻿import { styleProcess } from './winjs.js'
 import { dataBind, jsonLdBind } from './schema-binding.js'
 import { Me, ld } from './me.js'
+import './diagnal-ribbon.js'
 
 dataBind(document.querySelector("html") as HTMLElement, Me);
 jsonLdBind(ld, document);
