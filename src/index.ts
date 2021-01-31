8971aa3c9bc51068a0f2feec56d038d78b28a9f2
Shmueli Englard <shmueli.yosef@englard.net>
2021-01-31 17:04:53 -0500

    Changed to manually add WinJS classes

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,7 @@
-﻿import { styleProcess } from './winjs.js'
-import { dataBind, jsonLdBind } from './schema-binding.js'
+﻿import { dataBind, jsonLdBind } from './schema-binding.js'
 import { Me } from './me.js'
 import './diagnal-ribbon.js'
 import { Thing } from './schema.js';
 
 dataBind(document.querySelector("html") as HTMLElement, Me as Thing);
 jsonLdBind(Me, document);
-
-styleProcess(document.body);
