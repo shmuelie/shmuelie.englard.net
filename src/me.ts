75f1bfcd34b6464348dd13af1d1a21240c1145e0
Shmueli Englard <shmueli.yosef@englard.net>
2021-06-15 10:36:59 -0400

    Added twitch and removed name

--- src/me.ts
+++ src/me.ts
@@ -20,58 +20,56 @@ export const Me: WithContext<Person> = {
         {
             "@type": "ContactPoint",
             contactType: "Twitter",
-            name: "@Shmuelie",
             url: "https://twitter.com/Shmuelie",
             identifier: "Shmuelie"
         },
         {
             "@type": "ContactPoint",
             contactType: "LinkedIn",
-            name: "Shmueli Englard",
             url: "https://www.linkedin.com/in/shmuelienglard",
             identifier: "shmuelienglard"
         },
         {
             "@type": "ContactPoint",
             contactType: "GitHub",
-            name: "Samuel Englard",
             url: "https://github.com/SamuelEnglard",
             identifier: "SamuelEnglard"
         },
         {
             "@type": "ContactPoint",
             contactType: "StackOverflow",
-            name: "shmuelie",
             url: "https://stackoverflow.com/users/1082492/shmuelie",
             identifier: "shmuelie"
         },
         {
             "@type": "ContactPoint",
             contactType: "Facebook",
-            name: "Shmueli Englard",
             url: "https://www.facebook.com/Shmuelie/",
             identifier: "shmuelie"
         },
         {
             "@type": "ContactPoint",
             contactType: "NuGet",
-            name: "Shmuelie",
             url: "https://www.nuget.org/profiles/shmuelie",
             identifier: "shmuelie"
         },
         {
             "@type": "ContactPoint",
             contactType: "NPM",
-            name: "Shmueli Englard",
             url: "https://www.npmjs.com/~shmuelie",
             identifier: "shmuelie"
         },
         {
             "@type": "ContactPoint",
             contactType: "Instagram",
-            name: "Shmueli Englard",
             url: "https://www.instagram.com/shmuelienglard",
             identifier: "shmuelienglard"
+        },
+        {
+            "@type": "ContactPoint",
+            contactType: "Twitch",
+            url: "https://twitch.com/shmuelie",
+            identifier: "shmuelie"
         }
     ],
     description: "Cutting my programming chops on the .NET 2.0 Betas in 2005, I grew to love developing software. I started out using VB.NET but moved on to C# quickly and never looked back. While most of my work is in C#, I have professional experience with cross platform development in Java, C++ (SWIG); web development in JavaScript, node.js and, database development with MS SQL Server, PostgreSQL and MySQL, and data integration services using different transport protocols using JSON, GeoJSON, XML and Google Protocol Buffers.",
