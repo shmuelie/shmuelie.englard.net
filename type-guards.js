014d96ad5a14960c163840e8da630964987d3081
Shmueli <shmueli.yosef@englard.net>
2023-10-29 20:57:24 +0000

    Updating GH Pages

--- type-guards.js
+++
@@ -1,2 +0,0 @@
-export function isSchemaType(e,t){return"object"==typeof e&&"@type"in e&&e["@type"]===t}export function isArray(e){return"object"==typeof e&&Array.isArray(e)}export function isElement(e,t){return e.tagName===t.toUpperCase()}
-//# sourceMappingURL=type-guards.js.map
