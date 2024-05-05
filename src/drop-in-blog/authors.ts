7a0e6b5b9810233f4da7d338104c7153b210941e
Shmueli Englard <shmueli.yosef@englard.net>
2024-05-05 14:05:02 -0400

    Added authors API

--- src/drop-in-blog/authors.ts
+++ src/drop-in-blog/authors.ts
@@ -1,5 +1,16 @@
 import { operations } from '../../data/dropinblog.api';
+import { ErrorResponse, orError, get, rootUrl, isError } from './request-helper.js';
 
 export type Response = operations['authors-list']['responses']['200']['content']['application/json'];
 export type Data = NonNullable<Response['data']>;
 export type Author = NonNullable<Data['authors']>[0];
+
+export async function getAuthors(): Promise<Author[] | ErrorResponse> {
+    const response = orError<Data, Response>(await get<Response>(new URL(`${rootUrl}/authors`)), {
+        authors: []
+    });
+    if (isError(response)) {
+        return response;
+    }
+    return response.authors ?? [];
+}
