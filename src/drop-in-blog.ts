8a6e9bdd8866a001c661a96bef3579d8c578a5ec
Shmueli Englard <shmueli.yosef@englard.net>
2024-05-04 21:36:46 -0400

    Refactor to reduce duplicated code

--- src/drop-in-blog.ts
+++ src/drop-in-blog.ts
@@ -16,39 +16,33 @@ export type Author = PostsResponseAuthor | PostResponseAuthor;
 const blogId = 'f56590c5-56ae-4aab-8d55-df9c76db569c';
 const oauthKey = '';
 
+async function get<T>(url: URL): Promise<T> {
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            accept: 'application/json',
+            authorization: `Bearer ${oauthKey}`
+        }
+    });
+    return await response.json() as T;
+}
+
 /**
  * Fetch a paginated collection of posts. By default, only posts that are published and accessible to the public will be returned.
  * @param options Optional options for the request.
  * @returns A collection of posts.
  */
 export async function getPosts(options: PostsOptions = {}): Promise<PostsResponse> {
-    const requestOptions: RequestInit = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            authorization: `Bearer ${oauthKey}`
-        }
-    };
     const requestUrl: URL = new URL(`https://api.dropinblog.com/v2/blog/${blogId}/posts`);
     const optionsMap = options as {[k:string]:any};
     for (const optionName of Object.keys(options)) {
         requestUrl.searchParams.append(optionName, optionsMap[optionName]);
     }
-    const response = await fetch(requestUrl, requestOptions);
-    return await response.json() as PostsResponse;
+    return await get<PostsResponse>(requestUrl);
 }
 
 export async function getPost(id: number): Promise<PostResponse> {
-    const requestOptions: RequestInit = {
-        method: 'GET',
-        headers: {
-            accept: 'application/json',
-            authorization: `Bearer ${oauthKey}`
-        }
-    };
-    const requestUrl = new URL(`https://api.dropinblog.com/v2/blog/${blogId}/posts/${id}`);
-    const response = await fetch(requestUrl, requestOptions);
-    return await response.json() as PostResponse;
+    return await get<PostResponse>(new URL(`https://api.dropinblog.com/v2/blog/${blogId}/posts/${id}`));
 }
 
 export function convertAuthor(author: Author | undefined) : Person | undefined {
